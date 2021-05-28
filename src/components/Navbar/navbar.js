d2c93f152f23814188af65cad6b4a12dd6c1c463
jany-coder <jany_ete@yahoo.com>
2021-05-29 03:37:07 +0600

    Navbar edit

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -15,13 +15,11 @@ const NavBar = () => {
                         navbarScroll
                     >
                         <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#action2">About</Nav.Link>
+                        <Nav.Link href="#action2">Service</Nav.Link>
+                        <Nav.Link href="#action2">Contact</Nav.Link>
+                        <Nav.Link href="#action2"><b>Login</b></Nav.Link>
+                        
                     </Nav>
                 </Navbar.Collapse>
             </Container>
