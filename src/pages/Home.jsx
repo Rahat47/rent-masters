4970fc4d420a131aa10abf3af2109ab96a28dc11
Rahat47 <djrayhan8@gmail.com>
2021-05-28 23:14:36 +0600

    common header component done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/Navbar/navbar";
+import HotelsContainer from "../components/Hotels/HotelsContainer";
 
 const Home = () => {
     return (
         <div>
             <NavBar />
             <Header />
+            <HotelsContainer />
 
             <Footer />
         </div>
