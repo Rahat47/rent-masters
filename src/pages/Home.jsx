52999f7618093aa15488917192fd407ab497a9e5
Md Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-05-28 23:33:54 +0600

    add service section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/Navbar/navbar";
+import Service from "../components/Services/Service";
 
 const Home = () => {
     return (
         <div>
             <NavBar />
             <Header />
-
+            <Service/>
             <Footer />
         </div>
     );
