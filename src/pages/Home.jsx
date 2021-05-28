73985d741fe1f5abda1520935a5b6e54b7d17ff5
Rahat47 <djrayhan8@gmail.com>
2021-05-28 22:31:12 +0600

    navbar styles added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import NavBar from "../components/Navbar/navbar";
 
 const Home = () => {
     return (
         <div>
+            <NavBar />
             <h1>Home page is changed</h1>
             <Header />
 
