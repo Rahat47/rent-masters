ca37eadb59642fe1b57d335eea18156bbc573de1
Md Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-05-28 21:42:21 +0600

    home merged

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-import Footer from "./Footer"
-=======
 import Header from "../components/Header/Header";
->>>>>>> a820078f43b15cd288687c0a60f3b5e53b799737
 
 const Home = () => {
     return (
         <div>
             <h1>Home page is changed</h1>
-<<<<<<< HEAD
-            <Footer/>
-=======
-
             <Header />
->>>>>>> a820078f43b15cd288687c0a60f3b5e53b799737
         </div>
     );
 };
