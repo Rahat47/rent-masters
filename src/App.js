ae0ef5aa271df08ca2504ebf99c098df949a670c
Rahat47 <djrayhan8@gmail.com>
2021-05-29 12:12:06 +0600

    app js updated

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/Navbar/navbar';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <Router>
+      <NavBar />
+
 
       <Switch>
         <Route exact path="/" component={Home} />
@@ -14,6 +18,7 @@ function App() {
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/product/:id" component={ProductDetails} />
       </Switch>
+      <Footer />
 
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,13 @@
-//import Footer from "./Footer"
-import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import HotelsContainer from "../components/Hotels/HotelsContainer";
-import NavBar from "../components/Navbar/navbar";
 import Service from "../components/Services/Service";
 
 const Home = () => {
     return (
         <div>
-            <NavBar />
             <Header />
             <HotelsContainer />
             <Service />
-            <Footer />
         </div>
     );
 };
