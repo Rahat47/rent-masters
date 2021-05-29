2b766f212856d21fee8ba2e4f63d4e4f5de711a7
Md Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-05-29 10:01:46 +0600

    add fontawsome icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,31 @@
+import {
+    faEnvelope,
+    faMapMarkerAlt,
+    faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styles from "./Footer.module.css";
-
 const Footer = () => {
     return (
         <div className={styles.container}>
             <div className="container py-5">
                 <div className="row">
                     <div className="col-md-5">
-                        H#340 (4th Floor), Road #24, New DOHS, Mohakhali, Dhaka,
-                        Bangladesh 
-                        <ul className='list-unstyled'>
-                            <li>Phone: 018XXXXXXXX</li>
-                            <li>E-mail: info@company.com</li>
+                        <FontAwesomeIcon icon={faMapMarkerAlt} /> H#340 (4th
+                        Floor), Road #24, New DOHS, Mohakhali, <br /> Dhaka,
+                        Bangladesh
+                        <ul className="list-unstyled">
+                            <li>
+                                {" "}
+                                <FontAwesomeIcon icon={faPhone} /> Phone:
+                                018XXXXXXXX
+                            </li>
+                            <li>
+                                {" "}
+                                <FontAwesomeIcon icon={faEnvelope} /> E-mail:
+                                info@company.com
+                            </li>
                         </ul>
                     </div>
                     <div className="col-md-2">
@@ -40,25 +54,25 @@ const Footer = () => {
                         Sydney, with agents available to answer any question
                         24/7 <br />
                         <div className="mt-3">
-                            <a className="me-2" href="facebook">
+                            <a className="me-2" href="facebook.com">
                                 <img
                                     src="https://img.icons8.com/office/40/000000/facebook-circled.png"
                                     alt="facebook"
                                 />
                             </a>
-                            <a className="me-2" href="linkedin">
+                            <a className="me-2" href="linkedin.com">
                                 <img
                                     src="https://img.icons8.com/fluent/48/000000/linkedin-circled.png"
                                     alt="linkedin"
                                 />
                             </a>
-                            <a className="me-2" href="whatsapp">
+                            <a className="me-2" href="whatsapp.com">
                                 <img
                                     src="https://img.icons8.com/office/40/000000/whatsapp--v3.png"
                                     alt="whatsapp"
                                 />
                             </a>
-                            <a className="me-2" href="youtube">
+                            <a className="me-2" href="youtube.com">
                                 <img
                                     src="https://img.icons8.com/fluent/48/000000/youtube-play.png"
                                     alt="youtube"
