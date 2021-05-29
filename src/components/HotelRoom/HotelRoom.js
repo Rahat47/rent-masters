eff3efa6cb487710631fc882c2d9b71c0c26fffa
jany-coder <jany_ete@yahoo.com>
2021-05-29 13:22:02 +0600

    header done

--- src/components/HotelRoom/HotelRoom.js
+++ src/components/HotelRoom/HotelRoom.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './HotelRoom.css';
 
 const HotelRoom = () => {
     return (
         <div>
-            <h1>Hotel Room Details</h1>
+            <section className="header">
+                   <div className="hotel-text d-flex  align-items-center justify-content-center"> <h1>Hotel Details</h1>
+                   </div>
+            </section>
         </div>
     );
 };
