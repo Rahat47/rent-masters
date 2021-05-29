d87a564cf6538bf02fdbed3716ce25ce093f381f
jany-coder <jany_ete@yahoo.com>
2021-05-29 19:23:49 +0600

    data update

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -17,62 +17,69 @@ export const hotelsInfo = [
         capacity: 1,
         bedType: 'Double',
         bathroom: 1,
-        price: 2000
+        price: 2000,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio consequuntur fugit molestias cumque debitis. Suscipit eius aperiam praesentium impedit, exercitationem dolorum, totam dicta harum quasi repellendus fuga eos maiores mollitia culpa optio obcaecati sunt labore corrupti tempore consequuntur beatae dolorem quis eveniet. Officiis, doloribus dolore officia sapiente laudantium placeat ea perspiciatis odio dolor, minima minus rerum? Quisquam corrupti, eveniet tempore neque aspernatur ea rerum veniam eum! Aut porro quaerat laboriosam recusandae officia sapiente, doloremque eos ipsam hic neque nemo? Iusto, nemo officia molestias enim repudiandae sint ad sunt eaque dolores natus tempore voluptate omnis? Molestias temporibus illum dolorum qui recusandae vitae eos quisquam quos eum sapiente minus dicta sequi pariatur, cumque quod rem, ad aut officia soluta laudantium! Enim vel nisi perspiciatis dolorem laborum eius deserunt molestias voluptatum iste, voluptates quisquam reiciendis animi? Voluptates placeat nam consequuntur eos mollitia? Atque ex voluptatibus aut excepturi culpa modi iusto molestias nobis quam."
     },
     {
         id: uuidv4(),
-        title: 'Radisson Blu Chattogram',
+        title: 'Hotel Agrabad',
         img: hotelImg2,
         address: 'Plot # 02, Road # 01, O.R. Nizam Road, Chittagong',
         bed: 1,
         capacity: 1,
         bedType: 'Double',
         bathroom: 1,
-        price: 1800
+        price: 1800,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio consequuntur fugit molestias cumque debitis. Suscipit eius aperiam praesentium impedit, exercitationem dolorum, totam dicta harum quasi repellendus fuga eos maiores mollitia culpa optio obcaecati sunt labore corrupti tempore consequuntur beatae dolorem quis eveniet. Officiis, doloribus dolore officia sapiente laudantium placeat ea perspiciatis odio dolor, minima minus rerum? Quisquam corrupti, eveniet tempore neque aspernatur ea rerum veniam eum! Aut porro quaerat laboriosam recusandae officia sapiente, doloremque eos ipsam hic neque nemo? Iusto, nemo officia molestias enim repudiandae sint ad sunt eaque dolores natus tempore voluptate omnis? Molestias temporibus illum dolorum qui recusandae vitae eos quisquam quos eum sapiente minus dicta sequi pariatur, cumque quod rem, ad aut officia soluta laudantium! Enim vel nisi perspiciatis dolorem laborum eius deserunt molestias voluptatum iste, voluptates quisquam reiciendis animi? Voluptates placeat nam consequuntur eos mollitia? Atque ex voluptatibus aut excepturi culpa modi iusto molestias nobis quam."
+
     },
     {
         id: uuidv4(),
-        title: 'Radisson Blu Chattogram',
+        title: 'Well Park Residence',
         img: hotelImg3,
         address: 'Bulbul Centre, 486/B, O.R.Nizam Road, CDA Avenue',
         bed: 1,
         capacity: 1,
         bedType: 'Single',
         bathroom: 1,
-        price: 1200
+        price: 1200,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio consequuntur fugit molestias cumque debitis. Suscipit eius aperiam praesentium impedit, exercitationem dolorum, totam dicta harum quasi repellendus fuga eos maiores mollitia culpa optio obcaecati sunt labore corrupti tempore consequuntur beatae dolorem quis eveniet. Officiis, doloribus dolore officia sapiente laudantium placeat ea perspiciatis odio dolor, minima minus rerum? Quisquam corrupti, eveniet tempore neque aspernatur ea rerum veniam eum! Aut porro quaerat laboriosam recusandae officia sapiente, doloremque eos ipsam hic neque nemo? Iusto, nemo officia molestias enim repudiandae sint ad sunt eaque dolores natus tempore voluptate omnis? Molestias temporibus illum dolorum qui recusandae vitae eos quisquam quos eum sapiente minus dicta sequi pariatur, cumque quod rem, ad aut officia soluta laudantium! Enim vel nisi perspiciatis dolorem laborum eius deserunt molestias voluptatum iste, voluptates quisquam reiciendis animi? Voluptates placeat nam consequuntur eos mollitia? Atque ex voluptatibus aut excepturi culpa modi iusto molestias nobis quam."
     },
     {
         id: uuidv4(),
-        title: 'Radisson Blu Chattogram',
+        title: 'The Peninsula',
         img: hotelImg4,
         address: 'M M Ali Road. GEC More, T M T Ismail Tower 11/1',
         bed: 1,
         capacity: 1,
         bedType: 'Double',
         bathroom: 1,
-        price: 2200
+        price: 2200,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio consequuntur fugit molestias cumque debitis. Suscipit eius aperiam praesentium impedit, exercitationem dolorum, totam dicta harum quasi repellendus fuga eos maiores mollitia culpa optio obcaecati sunt labore corrupti tempore consequuntur beatae dolorem quis eveniet. Officiis, doloribus dolore officia sapiente laudantium placeat ea perspiciatis odio dolor, minima minus rerum? Quisquam corrupti, eveniet tempore neque aspernatur ea rerum veniam eum! Aut porro quaerat laboriosam recusandae officia sapiente, doloremque eos ipsam hic neque nemo? Iusto, nemo officia molestias enim repudiandae sint ad sunt eaque dolores natus tempore voluptate omnis? Molestias temporibus illum dolorum qui recusandae vitae eos quisquam quos eum sapiente minus dicta sequi pariatur, cumque quod rem, ad aut officia soluta laudantium! Enim vel nisi perspiciatis dolorem laborum eius deserunt molestias voluptatum iste, voluptates quisquam reiciendis animi? Voluptates placeat nam consequuntur eos mollitia? Atque ex voluptatibus aut excepturi culpa modi iusto molestias nobis quam."
     },
     {
         id: uuidv4(),
-        title: 'Radisson Blu Chattogram',
+        title: 'Jatra Rooms',
         img: hotelImg5,
         address: 'Ispahani Moor, Lalkhan Bazar, 4150 Chittagong',
         bed: 1,
         capacity: 1,
         bedType: 'Double',
         bathroom: 1,
-        price: 1900
+        price: 1900,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio consequuntur fugit molestias cumque debitis. Suscipit eius aperiam praesentium impedit, exercitationem dolorum, totam dicta harum quasi repellendus fuga eos maiores mollitia culpa optio obcaecati sunt labore corrupti tempore consequuntur beatae dolorem quis eveniet. Officiis, doloribus dolore officia sapiente laudantium placeat ea perspiciatis odio dolor, minima minus rerum? Quisquam corrupti, eveniet tempore neque aspernatur ea rerum veniam eum! Aut porro quaerat laboriosam recusandae officia sapiente, doloremque eos ipsam hic neque nemo? Iusto, nemo officia molestias enim repudiandae sint ad sunt eaque dolores natus tempore voluptate omnis? Molestias temporibus illum dolorum qui recusandae vitae eos quisquam quos eum sapiente minus dicta sequi pariatur, cumque quod rem, ad aut officia soluta laudantium! Enim vel nisi perspiciatis dolorem laborum eius deserunt molestias voluptatum iste, voluptates quisquam reiciendis animi? Voluptates placeat nam consequuntur eos mollitia? Atque ex voluptatibus aut excepturi culpa modi iusto molestias nobis quam."
     },
     {
         id: uuidv4(),
-        title: 'Radisson Blu Chattogram',
+        title: 'The Avenue Hotel',
         img: hotelImg6,
         address: '431, D.T. Road, Kadamtoli, 4100 Chittagong',
         bed: 1,
         capacity: 1,
         bedType: 'Single',
         bathroom: 1,
-        price: 1100
+        price: 1100,
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio consequuntur fugit molestias cumque debitis. Suscipit eius aperiam praesentium impedit, exercitationem dolorum, totam dicta harum quasi repellendus fuga eos maiores mollitia culpa optio obcaecati sunt labore corrupti tempore consequuntur beatae dolorem quis eveniet. Officiis, doloribus dolore officia sapiente laudantium placeat ea perspiciatis odio dolor, minima minus rerum? Quisquam corrupti, eveniet tempore neque aspernatur ea rerum veniam eum! Aut porro quaerat laboriosam recusandae officia sapiente, doloremque eos ipsam hic neque nemo? Iusto, nemo officia molestias enim repudiandae sint ad sunt eaque dolores natus tempore voluptate omnis? Molestias temporibus illum dolorum qui recusandae vitae eos quisquam quos eum sapiente minus dicta sequi pariatur, cumque quod rem, ad aut officia soluta laudantium! Enim vel nisi perspiciatis dolorem laborum eius deserunt molestias voluptatum iste, voluptates quisquam reiciendis animi? Voluptates placeat nam consequuntur eos mollitia? Atque ex voluptatibus aut excepturi culpa modi iusto molestias nobis quam."
     },
 
 ]
