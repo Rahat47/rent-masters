ade4e1d7cbf9241d028e7e6c860f1918dc262528
jany-coder <jany_ete@yahoo.com>
2021-05-30 10:28:17 +0600

    update hotel des

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -4,12 +4,11 @@ import hotelImg3 from '../images/The-Peninsula.jpg';
 import hotelImg4 from '../images/Jatra-Rooms.jpg';
 import hotelImg5 from '../images/The-Avenue-Hotel.jpg';
 import hotelImg6 from '../images/Royal-Living-Hotel.jpg';
-import { v4 as uuidv4 } from 'uuid'
 
 //Export the hotels info array from here
 export const hotelsInfo = [
     {
-        id: uuidv4(),
+        id: "60b281e6fc13ae7a4800163e",
         title: 'Radisson Blu Chattogram',
         img: hotelImg1,
         address: 'SS Khaled Road Lalkhan Bazar, Chittagong',
@@ -17,12 +16,12 @@ export const hotelsInfo = [
         capacity: 1,
         bedType: 'Double',
         bathroom: 1,
-        price: 2000, 
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt inventore fugit velit atque rem accusantium, est possimus doloribus voluptate in deserunt tempora distinctio similique, aliquam, esse modi quia asperiores animi enim nam temporibus nostrum sed quod fugiat? Possimus ipsa inventore fugiat aperiam aliquam, nisi laudantium accusantium odio distinctio ea hic eligendi velit dolore dolor maxime exercitationem. Dolores sed voluptatibus vitae ullam delectus exercitationem explicabo quis? Neque labore quo itaque mollitia, est asperiores eos totam officiis suscipit esse numquam repudiandae dicta minima explicabo eum eveniet ratione illo ducimus cumque? Neque nemo distinctio sint nam corrupti excepturi quos ea amet suscipit omnis? Molestias, nihil maxime assumenda itaque corporis eligendi reprehenderit vel beatae tempore veritatis quidem consectetur suscipit magnam earum voluptatem, cumque minus blanditiis enim. Commodi modi corporis quas maxime totam, ipsam voluptatum quo id ipsa deserunt maiores minima, magni ducimus, rerum et labore amet voluptatibus? Velit dolorum incidunt atque? Nam, sapiente cumque!"
+        price: 2000,
+        description: "Featuring a fitness centre, Radisson Blu Chittagong Bay View offers accommodation in Chittagong. Guests can enjoy the on-site multi-cuisine restaurant. Free WiFi is available throughout the property.Rooms come with a flat-screen satellite TV and a seating area for your convenience. All rooms have views of the mountains or city. Every room is equipped with a private bathroom fitted with a shower.You will find a 24-hour front desk and concierge desk at the property. Free parking is offered.The Chittagong Cricket Stadium is 7 km, the Khejur Tola Beach is 15 km and the Patenga Sea Beach is 16.2 km. The Halishahar Railway Station is 5.8 km. The Chittagong Bus Station is 9.8 km and the Shah Amanat International Airport is 14 km. The Shahjalal International Airport is 284 km.Couples particularly like the location — they rated it 9.3 for a two-person trip."
     },
     {
         id: uuidv4(),
-        title: 'Radisson Blu Chattogram',
+        title: 'Hotel Agrabad',
         img: hotelImg2,
         address: 'Plot # 02, Road # 01, O.R. Nizam Road, Chittagong',
         bed: 1,
@@ -30,10 +29,16 @@ export const hotelsInfo = [
         bedType: 'Double',
         bathroom: 1,
         price: 1800,
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt inventore fugit velit atque rem accusantium, est possimus doloribus voluptate in deserunt tempora distinctio similique, aliquam, esse modi quia asperiores animi enim nam temporibus nostrum sed quod fugiat? Possimus ipsa inventore fugiat aperiam aliquam, nisi laudantium accusantium odio distinctio ea hic eligendi velit dolore dolor maxime exercitationem. Dolores sed voluptatibus vitae ullam delectus exercitationem explicabo quis? Neque labore quo itaque mollitia, est asperiores eos totam officiis suscipit esse numquam repudiandae dicta minima explicabo eum eveniet ratione illo ducimus cumque? Neque nemo distinctio sint nam corrupti excepturi quos ea amet suscipit omnis? Molestias, nihil maxime assumenda itaque corporis eligendi reprehenderit vel beatae tempore veritatis quidem consectetur suscipit magnam earum voluptatem, cumque minus blanditiis enim. Commodi modi corporis quas maxime totam, ipsam voluptatum quo id ipsa deserunt maiores minima, magni ducimus, rerum et labore amet voluptatibus? Velit dolorum incidunt atque? Nam, sapiente cumque!"
+        description: "Featuring an all year outdoor swimming pool, Aqua Blu, the 4-star Hotel Agrabad also boasts a well-equipped gym and a bar with live band performances. Agrabad Thai Spa offers sauna, spa bath, foot massage, head, scalp and full body massage.Air-conditioned rooms are equipped with a flat-screen TV, minibar and personal safe. En suite bathrooms come with a shower.Hotel Agrabad is a 10-minute walk from downtown Chittagong and 5 km from the Main Sea Port. Shah Amanat International Airport is 20 km away.Guests can enjoy sheesha (flavoured tobacco) and Arabic tea at the relaxing Sheesha Lounge, next to the pool. The hotel also provides billiard tables and a tour desk.Cinnamon Restaurant serves Continental, while Oasis is a stylish Middle-Eastern style lounge and Pan- Asian for Chinese, Thai, Malay and Indian cuisine. Liquid Bar & Lounge serves alcoholic beverages.We speak your language!"
+
     },
     {
         id: uuidv4(),
+        title: 'Well Park Residence',
+        description: "Located in Chittagong, Well Park Residence Boutique Hotel & Suites features 4-star accommodation with private balconies. Among the facilities of this property are a restaurant, a 24-hour front desk and room service, along with free WiFi. Free private parking is available and the hotel also provides car hire for guests who want to explore the surrounding area.At the hotel, the rooms include a desk. At Well Park Residence Boutique Hotel, each room has air conditioning and a private bathroom.A buffet breakfast is available daily at the accommodation.The nearest airport is Shah Amanat International, 12 km from Well Park Residence Boutique Hotel & Suites, and the property offers a paid airport shuttle service.Couples particularly like the location — they rated it 8.1 for a two-person trip.We speak your language!"
+    },
+    {
+        id: "60b281e6fc13ae7a48001640",
         title: 'Radisson Blu Chattogram',
         img: hotelImg3,
         address: 'Bulbul Centre, 486/B, O.R.Nizam Road, CDA Avenue',
@@ -42,11 +47,11 @@ export const hotelsInfo = [
         bedType: 'Single',
         bathroom: 1,
         price: 1200,
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt inventore fugit velit atque rem accusantium, est possimus doloribus voluptate in deserunt tempora distinctio similique, aliquam, esse modi quia asperiores animi enim nam temporibus nostrum sed quod fugiat? Possimus ipsa inventore fugiat aperiam aliquam, nisi laudantium accusantium odio distinctio ea hic eligendi velit dolore dolor maxime exercitationem. Dolores sed voluptatibus vitae ullam delectus exercitationem explicabo quis? Neque labore quo itaque mollitia, est asperiores eos totam officiis suscipit esse numquam repudiandae dicta minima explicabo eum eveniet ratione illo ducimus cumque? Neque nemo distinctio sint nam corrupti excepturi quos ea amet suscipit omnis? Molestias, nihil maxime assumenda itaque corporis eligendi reprehenderit vel beatae tempore veritatis quidem consectetur suscipit magnam earum voluptatem, cumque minus blanditiis enim. Commodi modi corporis quas maxime totam, ipsam voluptatum quo id ipsa deserunt maiores minima, magni ducimus, rerum et labore amet voluptatibus? Velit dolorum incidunt atque? Nam, sapiente cumque!"
+        description: "Located in Chittagong, Well Park Residence Boutique Hotel & Suites features 4-star accommodation with private balconies. Among the facilities of this property are a restaurant, a 24-hour front desk and room service, along with free WiFi. Free private parking is available and the hotel also provides car hire for guests who want to explore the surrounding area.At the hotel, the rooms include a desk. At Well Park Residence Boutique Hotel, each room has air conditioning and a private bathroom.A buffet breakfast is available daily at the accommodation.The nearest airport is Shah Amanat International, 12 km from Well Park Residence Boutique Hotel & Suites, and the property offers a paid airport shuttle service.Couples particularly like the location — they rated it 8.1 for a two-person trip.We speak your language!"
     },
     {
         id: uuidv4(),
-        title: 'Radisson Blu Chattogram',
+        title: 'The Peninsula',
         img: hotelImg4,
         address: 'M M Ali Road. GEC More, T M T Ismail Tower 11/1',
         bed: 1,
@@ -54,11 +59,11 @@ export const hotelsInfo = [
         bedType: 'Double',
         bathroom: 1,
         price: 2200,
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt inventore fugit velit atque rem accusantium, est possimus doloribus voluptate in deserunt tempora distinctio similique, aliquam, esse modi quia asperiores animi enim nam temporibus nostrum sed quod fugiat? Possimus ipsa inventore fugiat aperiam aliquam, nisi laudantium accusantium odio distinctio ea hic eligendi velit dolore dolor maxime exercitationem. Dolores sed voluptatibus vitae ullam delectus exercitationem explicabo quis? Neque labore quo itaque mollitia, est asperiores eos totam officiis suscipit esse numquam repudiandae dicta minima explicabo eum eveniet ratione illo ducimus cumque? Neque nemo distinctio sint nam corrupti excepturi quos ea amet suscipit omnis? Molestias, nihil maxime assumenda itaque corporis eligendi reprehenderit vel beatae tempore veritatis quidem consectetur suscipit magnam earum voluptatem, cumque minus blanditiis enim. Commodi modi corporis quas maxime totam, ipsam voluptatum quo id ipsa deserunt maiores minima, magni ducimus, rerum et labore amet voluptatibus? Velit dolorum incidunt atque? Nam, sapiente cumque!"
+        description: "The Peninsula Chittagong Limited is a 4-star property in Port City's upscale GEC Circle, a short 5-minute walk from Central Plaza shopping area. An outdoor pool, pampering spa treatments and a well-equipped fitness centre are available. There is also a 24-hour front desk and free parking.The air-conditioned guestrooms all come with a flat-screen TV, minibar and personal safe. Separate smoking and non-smoking floors are available. En suite bathrooms are equipped with a shower.The Peninsula Chittagong Limited is a 20-minute drive from Chittagong Railway Station. Chittagong International Airport is a 45-minute drive away.Laguna Multi-Cuisine Restaurant offers dishes from Bangladesh, India, Thailand and China. Other dining options include Continental and Korean food at Flamingo Cafe, pastries at Orchid Patisserie and cocktails at Isles Bar.At the spa, guests can relax with a massage, or at the sauna and hot tub. The hotel also provides a business centre and a tour desk.Couples particularly like the location — they rated it 9.0 for a two-person trip.We speak your language!"
     },
     {
         id: uuidv4(),
-        title: 'Radisson Blu Chattogram',
+        title: 'Jatra Rooms',
         img: hotelImg5,
         address: 'Ispahani Moor, Lalkhan Bazar, 4150 Chittagong',
         bed: 1,
@@ -66,11 +71,11 @@ export const hotelsInfo = [
         bedType: 'Double',
         bathroom: 1,
         price: 1900,
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt inventore fugit velit atque rem accusantium, est possimus doloribus voluptate in deserunt tempora distinctio similique, aliquam, esse modi quia asperiores animi enim nam temporibus nostrum sed quod fugiat? Possimus ipsa inventore fugiat aperiam aliquam, nisi laudantium accusantium odio distinctio ea hic eligendi velit dolore dolor maxime exercitationem. Dolores sed voluptatibus vitae ullam delectus exercitationem explicabo quis? Neque labore quo itaque mollitia, est asperiores eos totam officiis suscipit esse numquam repudiandae dicta minima explicabo eum eveniet ratione illo ducimus cumque? Neque nemo distinctio sint nam corrupti excepturi quos ea amet suscipit omnis? Molestias, nihil maxime assumenda itaque corporis eligendi reprehenderit vel beatae tempore veritatis quidem consectetur suscipit magnam earum voluptatem, cumque minus blanditiis enim. Commodi modi corporis quas maxime totam, ipsam voluptatum quo id ipsa deserunt maiores minima, magni ducimus, rerum et labore amet voluptatibus? Velit dolorum incidunt atque? Nam, sapiente cumque!"
+        description: "Jatra Rooms CTG01 provides air-conditioned accommodation in Chittagong. All rooms feature a flat-screen TV with satellite channels and a private bathroom. Every room has a balcony.At the hotel, each room is fitted with a desk. All units will provide guests with a microwave.An Asian breakfast can be enjoyed at the property.Staff at the reception can provide guidance on what to do in the area.We speak your language!Jatra Rooms CTG01 provides air-conditioned accommodation in Chittagong. All rooms feature a flat-screen TV with satellite channels and a private bathroom. Every room has a balcony.At the hotel, each room is fitted with a desk. All units will provide guests with a microwave.An Asian breakfast can be enjoyed at the property.Staff at the reception can provide guidance on what to do in the area.We speak your language!"
     },
     {
         id: uuidv4(),
-        title: 'Radisson Blu Chattogram',
+        title: 'The Avenue Hotel',
         img: hotelImg6,
         address: '431, D.T. Road, Kadamtoli, 4100 Chittagong',
         bed: 1,
@@ -78,7 +83,7 @@ export const hotelsInfo = [
         bedType: 'Single',
         bathroom: 1,
         price: 1100,
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt inventore fugit velit atque rem accusantium, est possimus doloribus voluptate in deserunt tempora distinctio similique, aliquam, esse modi quia asperiores animi enim nam temporibus nostrum sed quod fugiat? Possimus ipsa inventore fugiat aperiam aliquam, nisi laudantium accusantium odio distinctio ea hic eligendi velit dolore dolor maxime exercitationem. Dolores sed voluptatibus vitae ullam delectus exercitationem explicabo quis? Neque labore quo itaque mollitia, est asperiores eos totam officiis suscipit esse numquam repudiandae dicta minima explicabo eum eveniet ratione illo ducimus cumque? Neque nemo distinctio sint nam corrupti excepturi quos ea amet suscipit omnis? Molestias, nihil maxime assumenda itaque corporis eligendi reprehenderit vel beatae tempore veritatis quidem consectetur suscipit magnam earum voluptatem, cumque minus blanditiis enim. Commodi modi corporis quas maxime totam, ipsam voluptatum quo id ipsa deserunt maiores minima, magni ducimus, rerum et labore amet voluptatibus? Velit dolorum incidunt atque? Nam, sapiente cumque!"
+        description: "THE AVENUE HOTEL & SUITES is located in Chittagong. Among the facilities of this property are a restaurant, a 24-hour front desk and room service, along with free WiFi. Private parking can be arranged at an extra charge.Guests at the hotel can enjoy a buffet breakfast.The nearest airport is Shah Amanat International Airport, 11 km from THE AVENUE HOTEL & SUITES.We speak your language!Laguna Multi-Cuisine Restaurant offers dishes from Bangladesh, India, Thailand and China. Other dining options include Continental and Korean food at Flamingo Cafe, pastries at Orchid Patisserie and cocktails at Isles Bar.At the spa, guests can relax with a massage, or at the sauna and hot tub. The hotel also provides a business centre and a tour desk."
     },
 
 ]
